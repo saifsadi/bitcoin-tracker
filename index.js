85ce9a31a20be5d2f666e1d85b244e3b66d80470
Saif Ur  Rehman <saifsadi5@gmail.com>
2019-03-31 21:14:10 +0500

    Add Crypto to Fiat converter by amount

--- index.js
+++ index.js
@@ -22,20 +22,35 @@ app.get("/", function(req, res){
   //body.crypto => crypto is the name of our first dropdown and fiat is the second
   var crypto = req.body.crypto;
   var fiat = req.body.fiat;
-  url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/'+crypto+fiat;
-
+  var amount = req.body.amount;
+  //old url => url = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/'+crypto+fiat;
+  //new api url = 'https://apiv2.bitcoinaverage.com/convert/global?from='+crypto+'&to='+fiat+'&amount='+amount;
   //use request method to send the http request and get data from bitcoinaverage api
 
-  request(url, function(err, response, body){
+
+  //options method in request package
+  var options = {
+    url : 'https://apiv2.bitcoinaverage.com/convert/global',
+    method : 'GET',
+    //qs meaning parameters after '?' in the base_url
+    qs : {
+      from : crypto,
+      to : fiat,
+      amount : amount
+    }
+  };
+
+
+  request(options, function(err, response, body){
       console.log("Error :" + err);
       console.log("Response :" + response.statusCode);
       console.log("Body :" + body);
 
       //converting JSON to javascript normal objects to get the data
       var data = JSON.parse(body);
-      var price = data.last;
-      res.write("<p>The current Date is " + data.display_timestamp + "</p>");
-      res.write("<h3> The Current Price of " + crypto + " is " + price + " " +fiat + "</h3>");
+      var price = data.price;
+      res.write("<p>The current Date is " + data.time + "</p>");
+      res.write("<h3> The " + amount + " " + crypto + " is currently worth " + price + " " +fiat + "</h3>");
       res.send();
 
   });
